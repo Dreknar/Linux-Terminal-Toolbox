7eed2e7a67ab9788204a17c65cad8fb8c501fde6
Piotr Chudzik <chk.piotr@gmail.com>
2022-07-05 09:54:05 +0000

    Init commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Linux Terminal Toolbox
+Repository includes tools and tweaks that are used when works with Linux using SSH connection or terminal.
+
+## How to use
+### Dependencies
+- [Nerd Fonts](https://www.nerdfonts.com/) (for some options)
+- Your favourite SSH Client / Terminal
+
+### Installation
+Just clone repository on your machine
+
+```
+git clone https://github.com/Dreknar/Linux-Terminal-Toolbox.git
+```
---
+++ pimp_my_term.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo '========== Update and upgrade system =========='
+sudo apt update && sudo apt upgrade -y
+
+echo '========== Install ZSH and oh my ZSH. =========='
+sudo apt install zsh -y
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+sudo apt install zsh-autosuggestions zsh-syntax-highlighting -y
+
+echo '========== Install extra tools =========='
+cd /tmp
+wget https://github.com/Peltoche/lsd/releases/download/0.22.0/lsd_0.22.0_amd64.deb
+sudo dpkg -i lsd_0.22.0_amd64.deb
+
+wget https://github.com/sharkdp/bat/releases/download/v0.21.0/bat_0.21.0_amd64.deb
+sudo dpkg -i bat_0.21.0_amd64.deb
+
+wget https://github.com/aristocratos/btop/releases/download/v1.2.8/btop-x86_64-linux-musl.tbz
+mkdir btop
+sudo tar -xvf btop-x86_64-linux-musl.tbz -C btop
+sudo ./btop/install.sh
+
+sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /usr/share/zsh-theme-powerlevel10k
