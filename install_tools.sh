a126eff8cab189977fb7542579f039458fb4bb95
Piotr Chudzik <chk.piotr@gmail.com>
2022-07-11 11:11:30 +0000

    Fix problem with pyenv install in install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -27,6 +27,11 @@ if [[ $1 == 'micro' ]]; then
 fi
 
 if [[ $1 == 'pyenv' ]]; then
+    sudo apt-get update
+    sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+
     curl https://pyenv.run | bash
 
     echo -e '\nexport PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
